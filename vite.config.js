fa48d3eebdb2aa654849374582ded1bd2de427e5
Gemnet <ssomani.k@gmail.com>
2024-09-30 02:27:00 -0400

    Updates

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  base: '/e-plantShopping/', // Ensure this matches your GitHub repository name
-  plugins: [react()],
-});
