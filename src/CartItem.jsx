555cc04df37e93fd3845fbf59a3fccee179d97a3
gemnets <154671337+gemnets@users.noreply.github.com>
2024-10-02 17:47:49 -0400

    Update CartItem.jsx

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -70,9 +70,9 @@ const CartItem = ({ onContinueShopping }) => {
                 )}
             </div>
             <div className="continue_shopping_btn">
-                <button className="get-started-button" onClick={handleContinueShopping}>Continue Shopping</button>
+                <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
                 <br />
-                <button className="get-started-button1" onClick={() => alert('Coming Soon')}>Checkout</button>
+                <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
             </div>
         </div>
     );
